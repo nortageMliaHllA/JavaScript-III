b72fd46c7f9f376d1cd93554c46b04854da2636a
AllHailMegatron <ahmadrthomp@gmail.com>
2019-05-07 19:09:04 -0400

    Completed this.js assignment, beginning prototypes.js

--- assignments/this.js
+++ assignments/this.js
@@ -33,7 +33,44 @@ myObj.sayHello('Megatron');
 // Principle 3
 
 // code example for New Binding
+function Parent(attr) {
+    this.name = attr.name;
+    this.age = attr.age;
+    this.phrase = attr.phrase; 
+}
+
+Parent.prototype.speak = function() {
+    return `${this.name} says ${this.phrase}`;
+}
+
+const ahmad = new Parent({
+    name: 'Ahmad',
+    age: 34,
+    phrase: 'We must learn JavaScript!'
+})
+
+const ashley = new Parent({
+    name: 'Ashley',
+    age: 31,
+    phrase: 'Ahmad!'
+})
+
+console.log(ahmad.speak());
+console.log(ashley.speak());
 
 // Principle 4
 
 // code example for Explicit Binding
+function Person(obj) {
+    this.name = obj.name;
+    this.age = obj.age;
+    this.speak = function() {
+        console.log(this);
+        return `Hello, my name is ${this.name} and I am ${this.age} years old!`;
+    }
+}
+const chris = new Person({name:'Chris', age: 30});
+const adam = new Person({name:'Adam', age: 24});
+
+chris.speak.call(adam);
+adam.speak.apply(chris);
